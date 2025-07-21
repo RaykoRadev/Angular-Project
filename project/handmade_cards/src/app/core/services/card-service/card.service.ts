d6598a81e4b19423a825cdf27237f80ca8e9f5b9
RaykoRadev <christmas898@gmail.com>
2025-07-21 17:29:21 +0300

    impelementing getting cards by category service

--- project/handmade_cards/src/app/core/services/card-service/card.service.ts
+++ project/handmade_cards/src/app/core/services/card-service/card.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BASE_CARD_URL } from '../../../shared/cosntants/constants';
-import { CardIntFull, CardResp } from '../../../shared/utils/interfaces';
+import {
+  CardGetAllR,
+  CardIntFull,
+  CardResp,
+} from '../../../shared/utils/interfaces';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,9 +16,22 @@ export class CardService {
 
   create(data: CardIntFull): Observable<CardResp> {
     const url = BASE_CARD_URL + '/create';
-    console.log(data);
-    console.log('url:', url);
-    console.log('Data payload:', JSON.stringify(data, null, 2));
+    // console.log(data);
+    // console.log('url:', url);
+    // console.log('Data payload:', JSON.stringify(data, null, 2));
     return this.http.post<CardResp>(url, data);
   }
+
+  getByCategory(category: string): Observable<CardGetAllR> {
+    const url =
+      BASE_CARD_URL +
+      `?category=${category}&sortBy=createdAt&limit=10&order=asc`;
+    console.log(url);
+
+    return this.http.get<CardGetAllR>(url);
+  }
+
+  edit() {}
+
+  delete() {}
 }
