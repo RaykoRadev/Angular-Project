0d165b5e1591ea67c8e9c203e15b2f3fdb86bcce
RaykoRadev <christmas898@gmail.com>
2025-08-02 14:58:43 +0300

    some updates on the geolocation and update the angular.json

--- src/app/layout/contact-us/contact-us.component.ts
+++ src/app/layout/contact-us/contact-us.component.ts
@@ -1,9 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Geolocation } from '../../core/services/geolocation-service/geolocation.service';
 import * as L from 'leaflet';
-// import 'leaflet/dist/images/marker-icon.png';
-// import 'leaflet/dist/images/marker-icon-2x.png';
-// import 'leaflet/dist/images/marker-shadow.png';
 
 @Component({
   selector: 'app-contact-us',
@@ -12,43 +8,26 @@ import * as L from 'leaflet';
   styleUrl: './contact-us.css',
 })
 export class ContactUs implements AfterViewInit {
-  latitude: number | null = null;
-  longitude: number | null = null;
-
-  constructor(private geoService: Geolocation) {}
-
-  ngOnInit(): void {}
-
   ngAfterViewInit(): void {
-    this.geoService.getUserLocation().then((location) => {
-      this.latitude = location.lat;
-      this.longitude = location.lng;
-
-      const map = L.map('map').setView([this.latitude, this.longitude], 13);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors',
-      }).addTo(map);
-
-      const DefaultIcon = L.icon({
-        iconUrl: 'assets/leaflet/marker-icon.png',
-        iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
-        shadowUrl: 'assets/leaflet/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        this.initTheMap();
       });
+    }, 0);
+  }
 
-      L.Marker.prototype.options.icon = DefaultIcon;
+  initTheMap(): void {
+    const map = L.map('map').setView([43.24314, 26.5674], 13);
 
-      L.marker([this.latitude, this.longitude])
-        .addTo(map)
-        .bindPopup('Вие е сте тук!')
-        .openPopup();
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
 
-      setTimeout(() => {
-        map.invalidateSize();
-      }, 100);
-    });
+    L.marker([43.24314, 26.5674])
+      .addTo(map)
+      .bindPopup('Вие е сте тук!')
+      .openPopup();
+
+    setTimeout(() => map.invalidateSize(), 300);
   }
 }
